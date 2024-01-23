84d52f525a2cff458653498b11aa5a6562331eab
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-01-23 20:08:24 +1100

    Add graph of wireless signal

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
 	$dataPoints3 = array();
 	$rc = 0;
 
-	$query = "SELECT UNIX_TIMESTAMP(whentime) * 1000 as whentime,temperature,humidity,feelslike FROM fujitsu WHERE whentime >= now() - INTERVAL 2 DAY ORDER BY whentime ASC";
+	$query = "SELECT UNIX_TIMESTAMP(whentime) * 1000 as whentime,temperature,humidity,feelslike,rssi FROM fujitsu WHERE whentime >= now() - INTERVAL 2 DAY ORDER BY whentime ASC";
 	$res = mysqli_query($link, $query);
 	while($row = mysqli_fetch_assoc($res))
 	{
 		$dataPoints1[] = array('x' => $row['whentime'], 'y' => $row['temperature']);
 		$dataPoints2[] = array('x' => $row['whentime'], 'y' => $row['humidity']);
 		$dataPoints3[] = array('x' => $row['whentime'], 'y' => $row['feelslike']);
+		$dataPoints4[] = array('x' => $row['whentime'], 'y' => $row['rssi']);
 		$rc++;
 	}
 
@@ -80,6 +81,38 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
 chart.render();
 
+var chart2 = new CanvasJS.Chart("rssiContainer", {
+	animationEnabled: true,
+	title:{
+		text: "WIFI Signal Strength dBm"
+	},
+	axisX:{
+		title: "Time",
+	},
+	axisY:{
+		title: "Signal Strength [dBm]",
+		titleFontColor: "#4F81BC",
+		lineColor: "#4F81BC",
+		labelFontColor: "#4F81BC",
+		tickColor: "#4F81BC"
+	},
+	legend:{
+		cursor: "pointer",
+		dockInsidePlotArea: true,
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "line",
+		name: "Signal Strength dBm",
+		xValueType: "dateTime",
+		markerSize: 0,
+		toolTipContent: "{name}: {y} dBm",
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($dataPoints4, JSON_NUMERIC_CHECK); ?>
+        }]
+});
+chart2.render();
+
 function toggleDataSeries(e){
 	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
 		e.dataSeries.visible = false;
@@ -95,6 +128,7 @@ function toggleDataSeries(e){
 </head>
 <body>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="rssiContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 </body>
 </html>
