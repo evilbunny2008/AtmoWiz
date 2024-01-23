dc6716d188d6389511c566cc905c4a8bf3bfd1e7
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-01-24 06:42:30 +1100

    Tweaked the API call to only need one call instead of 2

--- sensibo_daemon.py
+++ sensibo_daemon.py
@@ -35,23 +35,20 @@ class SensiboClientAPI(object):
             return None
         return {x['room']['name']: x['id'] for x in result['result']}
 
-    def pod_ac_state(self, podUid):
-        result = self._get("/pods/%s/acStates" % podUid, limit = 1, fields="acState")
+    def pod_all_stats(self, podUid):
+        result = self._get("/pods/%s/acStates" % podUid, limit = 1, fields="*")
         if(result == None):
             return None
-        return result['result'][0]['acState']
-
-    def pod_measurement(self, podUid):
-        result = self._get("/pods/%s/measurements" % str(podUid))
-        if(result == None):
-            return None
-        return result['result']
+        return result
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Daemon to collect data from Sensibo.com and store it locally.')
-    parser.add_argument('-c', '--config', type = str, default='/etc/fujitsu.conf',help='Path to config file, /etc/fujitsu.conf is the default')
-    parser.add_argument('--logfile', type = str, default='/var/log/fujitsu/fujitsu.log',help='File to log output to, /var/log/fujitsu/fujitsu.log is the default')
-    parser.add_argument('--pidfile', type = str, default='/var/run/fujitsu/fujitsu.pid',help='File to set the pid to, /var/run/fujitsu/fujitsu.pid is the default')
+    parser.add_argument('-c', '--config', type = str, default='/etc/fujitsu.conf',
+                        help='Path to config file, /etc/fujitsu.conf is the default')
+    parser.add_argument('--logfile', type = str, default='/var/log/fujitsu/fujitsu.log',
+                        help='File to log output to, /var/log/fujitsu/fujitsu.log is the default')
+    parser.add_argument('--pidfile', type = str, default='/var/run/fujitsu/fujitsu.pid',
+                        help='File to set the pid to, /var/run/fujitsu/fujitsu.pid is the default')
     args = parser.parse_args()
 
     configParser = configparser.ConfigParser()
@@ -81,7 +78,8 @@ if __name__ == "__main__":
       old_pid = int(file.readline())
       pid = os.getpid()
       if(pid != old_pid):
-        print ("Sensibo daemon is already running with pid %d, and pidfile %s already exists, exiting..." % (old_pid, args.pidfile))
+        print ("""Sensibo daemon is already running with pid %d, \
+                  and pidfile %s already exists, exiting...""" % (old_pid, args.pidfile))
         exit(1)
     else:
       mydir = os.path.dirname(os.path.abspath(args.pidfile))
@@ -117,21 +115,24 @@ if __name__ == "__main__":
     uidList = devices.values()
 
     logfile = open(args.logfile, 'a')
-    context = daemon.DaemonContext(stdout = logfile, stderr = logfile, pidfile=pidfile.TimeoutPIDLockFile(args.pidfile), uid=uid, gid=gid)
+    context = daemon.DaemonContext(stdout = logfile, stderr = logfile,
+                                   pidfile=pidfile.TimeoutPIDLockFile(args.pidfile), uid=uid, gid=gid)
 
     with context:
       while True:
         mydb = pymysql.connect(hostname, username, password, database, cursorclass=pymysql.cursors.DictCursor)
-        sql = """INSERT INTO fujitsu (whentime, uid, temperature, humidity, feelslike, rssi, airconon, mode, targettemp, fanlevel, swing, horizontalswing) 
+        sql = """INSERT INTO fujitsu (whentime, uid, temperature, humidity, feelslike, rssi,
+                        airconon, mode, targettemp, fanlevel, swing, horizontalswing)
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         start = time.time()
 
         for uid in uidList:
-          pod_measurement = client.pod_measurement(uid)
+          pod_measurement = client.pod_all_stats(uid)
           if(pod_measurement == None):
             continue
-          ac_state = pod_measurement[0]
-          sstring = datetime.strptime(ac_state['time']['time'], fromfmt)
+          ac_state = pod_measurement['result'][0]['acState']
+          measurements = pod_measurement['result'][0]['device']['measurements']
+          sstring = datetime.strptime(measurements['time']['time'], fromfmt)
           utc = sstring.replace(tzinfo=from_zone)
           localzone = utc.astimezone(to_zone)
           sdate = localzone.strftime(fmt)
@@ -145,18 +146,10 @@ if __name__ == "__main__":
                 row = cursor.fetchone()
                 if(row):
                   continue
-              except pymysql.err.IntegrityError as e:
-                pass
 
-          ac_state2 = client.pod_ac_state(uid)
-          if(ac_state2 == None):
-            continue
-
-          with mydb:
-            with mydb.cursor() as cursor:
-              try:
-                values = (sdate, uid, ac_state['temperature'], ac_state['humidity'], ac_state['feelsLike'], ac_state['rssi'], ac_state2['on'], ac_state2['mode'],
-                                      ac_state2['targetTemperature'], ac_state2['fanLevel'], ac_state2['swing'], ac_state2['horizontalSwing'])
+                values = (sdate, uid, measurements['temperature'], measurements['humidity'], measurements['feelsLike'],
+                          measurements['rssi'], ac_state['on'], ac_state['mode'], ac_state['targetTemperature'],
+                          ac_state['fanLevel'], ac_state['swing'], ac_state['horizontalSwing'])
                 cursor.execute(sql, values)
                 print (sql % values)
               except pymysql.err.IntegrityError as e:
