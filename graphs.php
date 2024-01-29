d3474a5a3b7943d6cd87436c665faa92dfbafaff
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-01-30 01:13:36 +1100

    Add favicon.svg

--- graphs.php
+++ graphs.php
@@ -21,6 +21,7 @@
 <title>Sensibo Data Plotting</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="favicon.svg">
 <style>
 * {
     margin: 0;
--- index.php
+++ index.php
@@ -32,7 +32,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="favicon.svg">
 <style>
 * {
   margin: 0;
