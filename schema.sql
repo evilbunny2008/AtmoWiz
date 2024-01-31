1f5bdcffaf7b31b67d49d9d5a98f51d8722f796f
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-01 00:43:51 +1100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,26 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `sensibo`;
 USE `sensibo`;
 
-CREATE TABLE IF NOT EXISTS `sensibo` (
-  `whentime` datetime NOT NULL,
-  `uid` varchar(20) NOT NULL DEFAULT '',
-  `temperature` float NOT NULL DEFAULT 0,
-  `humidity` int(11) NOT NULL DEFAULT 0,
-  `feelslike` float NOT NULL DEFAULT 0,
-  `rssi` int(11) NOT NULL DEFAULT 0,
-  `airconon` tinyint(1) NOT NULL,
-  `mode` varchar(20) NOT NULL,
-  `targettemp` float NOT NULL,
-  `fanlevel` varchar(20) NOT NULL,
-  `swing` varchar(20) NOT NULL,
-  `horizontalswing` varchar(20) NOT NULL,
-  PRIMARY KEY (`whentime`),
-  KEY `uid` (`uid`)
-) ENGINE=InnoDB;
-
-ALTER TABLE `sensibo` DROP INDEX IF EXISTS `PRIMARY`;
-ALTER TABLE `sensibo` DROP INDEX IF EXISTS `uid`;
-ALTER TABLE `sensibo` ADD PRIMARY KEY (`whentime`), ADD KEY `uid` (`uid`);
+START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS `commands` (
   `whentime` datetime NOT NULL,
@@ -34,21 +15,41 @@ CREATE TABLE IF NOT EXISTS `commands` (
   `temperatureUnit` varchar(1) NOT NULL,
   `fanLevel` varchar(20) NOT NULL,
   `swing` varchar(20) NOT NULL,
-  `horizontalSwing` varchar(20) NOT NULL
+  `horizontalSwing` varchar(20) NOT NULL,
+  PRIMARY KEY (`whentime`),
+  KEY `uid` (`uid`)
 ) ENGINE=InnoDB;
-
-ALTER TABLE `commands` DROP INDEX IF EXISTS `PRIMARY`;
-ALTER TABLE `commands` DROP INDEX IF EXISTS `uid`;
-ALTER TABLE `commands` ADD PRIMARY KEY (`whentime`), ADD KEY `uid` (`uid`);
 
 CREATE TABLE IF NOT EXISTS `devices` (
   `uid` varchar(20) NOT NULL,
-  `name` varchar(20) NOT NULL
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB;
 
-ALTER TABLE `devices` DROP INDEX IF EXISTS `PRIMARY`;
-ALTER TABLE `devices` DROP INDEX IF EXISTS `name`;
-ALTER TABLE `devices` ADD PRIMARY KEY (`uid`), ADD KEY `name` (`name`);
+CREATE TABLE IF NOT EXISTS `meta` (
+  `uid` varchar(20) NOT NULL,
+  `mode` varchar(20) NOT NULL,
+  `keyval` varchar(20) NOT NULL,
+  `value` varchar(20) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `sensibo` (
+  `whentime` datetime NOT NULL,
+  `uid` varchar(20) NOT NULL DEFAULT '',
+  `temperature` float NOT NULL DEFAULT 0,
+  `humidity` int(11) NOT NULL DEFAULT 0,
+  `feelslike` float NOT NULL DEFAULT 0,
+  `rssi` int(11) NOT NULL DEFAULT 0,
+  `airconon` tinyint(1) NOT NULL DEFAULT 0,
+  `mode` varchar(20) NOT NULL DEFAULT 'cool',
+  `targetTemperature` float NOT NULL DEFAULT 0,
+  `fanLevel` varchar(20) NOT NULL DEFAULT 'medium',
+  `swing` varchar(20) NOT NULL DEFAULT 'fixedTop',
+  `horizontalSwing` varchar(20) NOT NULL DEFAULT 'fixedCenter',
+  PRIMARY KEY (`whentime`),
+  KEY `uid` (`uid`)
+) ENGINE=InnoDB;
 
 COMMIT;
 
