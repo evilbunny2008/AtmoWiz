3a787723ff0ba9eea3ae6cb338e7982fb1a4edc0
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-09 02:14:45 +1100

    Add JS code to sync graphs

--- web/graphs.php
+++ web/graphs.php
@@ -579,6 +579,102 @@ var chart3 = new CanvasJS.Chart("costContainer",
        	}],
 });
 
+var charts = [];
+charts.push(chart1);
+charts.push(chart2);
+charts.push(chart3);
+
+syncCharts(charts, true, true, true);
+
+function syncCharts(charts, syncToolTip, syncCrosshair, syncAxisXRange)
+{
+	if(!this.onToolTipUpdated)
+	{
+		this.onToolTipUpdated = function(e)
+		{
+			for(var j = 0; j < charts.length; j++)
+				if(charts[j] != e.chart)
+					charts[j].toolTip.showAtX(e.entries[0].xValue);
+		}
+	}
+
+	if(!this.onToolTipHidden)
+	{
+		this.onToolTipHidden = function(e)
+		{
+			for(var j = 0; j < charts.length; j++)
+				if(charts[j] != e.chart)
+					charts[j].toolTip.hide();
+		}
+	}
+
+	if(!this.onCrosshairUpdated)
+	{
+		this.onCrosshairUpdated = function(e)
+		{
+			for(var j = 0; j < charts.length; j++)
+				if(charts[j] != e.chart)
+					charts[j].axisX[0].crosshair.showAt(e.value);
+		}
+	}
+
+	if(!this.onCrosshairHidden)
+	{
+		this.onCrosshairHidden =  function(e)
+		{
+			for(var j = 0; j < charts.length; j++)
+				if(charts[j] != e.chart)
+					charts[j].axisX[0].crosshair.hide();
+		}
+	}
+
+	if(!this.onRangeChanged)
+	{
+		this.onRangeChanged = function(e)
+		{
+			for(var j = 0; j < charts.length; j++)
+			{
+				if(e.trigger === "reset")
+				{
+					charts[j].options.axisX.viewportMinimum = charts[j].options.axisX.viewportMaximum = null;
+					charts[j].options.axisY.viewportMinimum = charts[j].options.axisY.viewportMaximum = null;
+					charts[j].render();
+				} else if (charts[j] !== e.chart) {
+					charts[j].options.axisX.viewportMinimum = e.axisX[0].viewportMinimum;
+					charts[j].options.axisX.viewportMaximum = e.axisX[0].viewportMaximum;
+					charts[j].render();
+				}
+			}
+		}
+	}
+
+	for(var i = 0; i < charts.length; i++)
+	{
+		if(syncToolTip)
+		{
+			if(!charts[i].options.toolTip)
+				charts[i].options.toolTip = {};
+			charts[i].options.toolTip.updated = this.onToolTipUpdated;
+			charts[i].options.toolTip.hidden = this.onToolTipHidden;
+		}
+
+		if(syncCrosshair)
+		{
+			if(!charts[i].options.axisX)
+				charts[i].options.axisX = { crosshair: { enabled: true }};
+
+//			charts[i].options.axisX.crosshair.updated = this.onCrosshairUpdated;
+//			charts[i].options.axisX.crosshair.hidden = this.onCrosshairHidden;
+                }
+
+		if(syncAxisXRange)
+		{
+			charts[i].options.zoomEnabled = true;
+			charts[i].options.rangeChanged = this.onRangeChanged;
+		}
+	}
+}
+
 function toggleDataSeries(e)
 {
 	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible)
@@ -690,13 +786,11 @@ console.log(url);
 		chart1.options.data[0].dataPoints = content['dataPoints3'];
 		chart1.options.data[1].dataPoints = content['dataPoints2'];
 		chart1.options.data[2].dataPoints = content['dataPoints1'];
-		chart1.render();
-
 		chart2.options.data[0].dataPoints = content['dataPoints4'];
-		chart2.render();
-
 		chart3.options.data[0].dataPoints = content['dataPoints5'];
-		chart3.render();
+
+		for(var i = 0; i < charts.length; i++)
+			charts[i].render();
 
 		populateSelect();
 	} catch (e) {
