a912c8d70c5c5e109c6004a3d312bd269d11ee43
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-09 22:17:40 +1100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,48 +5,59 @@ USE `sensibo`;
 
 START TRANSACTION;
 
+CREATE DATABASE IF NOT EXISTS `sensibo`;
+USE `sensibo`;
+
 CREATE TABLE IF NOT EXISTS `commands` (
   `whentime` datetime NOT NULL,
-  `uid` varchar(20) NOT NULL,
+  `uid` varchar(8) NOT NULL,
   `reason` varchar(20) NOT NULL,
-  `who` varchar(255) NOT NULL,
-  `status` varchar(20) NOT NULL,
+  `who` varchar(30) NOT NULL,
+  `status` enum('Success','Failed') NOT NULL DEFAULT 'Success',
   `airconon` tinyint(1) NOT NULL,
-  `mode` varchar(20) NOT NULL,
+  `mode` enum('cool','heat','dry','auto','fan') NOT NULL DEFAULT 'cool',
   `targetTemperature` tinyint(4) NOT NULL,
   `temperatureUnit` varchar(1) NOT NULL,
-  `fanLevel` varchar(20) NOT NULL,
-  `swing` varchar(20) NOT NULL,
-  `horizontalSwing` varchar(20) NOT NULL,
-  `changes` varchar(255) NOT NULL
+  `fanLevel` enum('quiet','low','medium','high','auto') NOT NULL DEFAULT 'medium',
+  `swing` enum('stopped','fixedTop','fixedMiddleTop','fixedMiddleBottom','fixedBottom','rangeFull') NOT NULL DEFAULT 'fixedTop',
+  `horizontalSwing` enum('stopped','fixedLeft','fixedCenterLeft','fixedCenter','fixedCenterRight','fixedRight','rangeFull') NOT NULL DEFAULT 'fixedCenter',
+  `changes` varchar(50) NOT NULL,
+  PRIMARY KEY (`whentime`,`uid`) USING BTREE
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `devices` (
-  `uid` varchar(20) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `uid` varchar(8) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `meta` (
   `uid` varchar(20) NOT NULL,
   `mode` varchar(20) NOT NULL,
   `keyval` varchar(20) NOT NULL,
-  `value` varchar(20) NOT NULL
+  `value` varchar(20) NOT NULL,
+  KEY `uid` (`uid`),
+  KEY `mode` (`mode`),
+  KEY `keyval` (`keyval`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `sensibo` (
   `whentime` datetime NOT NULL,
-  `uid` varchar(20) NOT NULL DEFAULT '',
+  `uid` varchar(8) NOT NULL DEFAULT '',
   `temperature` float NOT NULL DEFAULT 0,
-  `humidity` int(11) NOT NULL DEFAULT 0,
+  `humidity` tinyint(3) NOT NULL DEFAULT 0,
   `feelslike` float NOT NULL DEFAULT 0,
-  `rssi` int(11) NOT NULL DEFAULT 0,
+  `rssi` tinyint(3) NOT NULL DEFAULT 0,
   `airconon` tinyint(1) NOT NULL DEFAULT 0,
-  `mode` varchar(20) NOT NULL DEFAULT 'cool',
-  `targetTemperature` float NOT NULL DEFAULT 0,
-  `fanLevel` varchar(20) NOT NULL DEFAULT 'medium',
-  `swing` varchar(20) NOT NULL DEFAULT 'fixedTop',
-  `horizontalSwing` varchar(20) NOT NULL DEFAULT 'fixedCenter',
-  `cost` float NOT NULL DEFAULT 0
+  `mode` enum('cool','heat','dry','auto','fan') NOT NULL DEFAULT 'cool',
+  `targetTemperature` smallint(6) NOT NULL DEFAULT 0,
+  `fanLevel` enum('quiet','low','medium','high','auto') NOT NULL DEFAULT 'medium',
+  `swing` enum('stopped','fixedTop','fixedMiddleTop','fixedMiddleBottom','fixedBottom','rangeFull') NOT NULL DEFAULT 'fixedTop',
+  `horizontalSwing` enum('stopped','fixedLeft','fixedCenterLeft','fixedCenter','fixedCenterRight','fixedRight','rangeFull') NOT NULL DEFAULT 'fixedCenter',
+  `cost` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`whentime`,`uid`) USING BTREE,
+  KEY `cost` (`cost`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `settings` (
@@ -56,17 +67,12 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `targetType` enum('temperature','humidity','feelsLike') NOT NULL DEFAULT 'temperature',
   `onValue` float NOT NULL DEFAULT 28,
   `offValue` float NOT NULL DEFAULT 26.1,
-  `targetTemperature` float NOT NULL DEFAULT 26,
+  `targetTemperature` smallint(6) NOT NULL DEFAULT 26,
   `fanLevel` enum('quiet','low','medium','high','auto') NOT NULL DEFAULT 'auto',
   `swing` enum('stopped','fixedTop','fixedMiddleTop','fixedMiddleBottom','fixedBottom','rangeFull') NOT NULL DEFAULT 'fixedTop',
   `horizontalSwing` enum('stopped','fixedLeft','fixedCenterLeft','fixedCenter','fixedCenterRight','fixedRight','rangeFull') NOT NULL DEFAULT 'fixedCenter',
-  `enabled` tinyint(1) NOT NULL DEFAULT 0
+  `enabled` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`uid`,`created`)
 ) ENGINE=InnoDB;
-
-ALTER TABLE `commands` ADD PRIMARY KEY IF NOT EXISTS (`whentime`,`uid`) USING BTREE;
-ALTER TABLE `devices` ADD PRIMARY KEY IF NOT EXISTS (`uid`), ADD KEY IF NOT EXISTS `name` (`name`);
-ALTER TABLE `meta` ADD KEY IF NOT EXISTS `uid` (`uid`), ADD KEY IF NOT EXISTS `mode` (`mode`), ADD KEY IF NOT EXISTS `keyval` (`keyval`);
-ALTER TABLE `sensibo` ADD PRIMARY KEY IF NOT EXISTS (`whentime`,`uid`) USING BTREE;
-ALTER TABLE `settings` ADD PRIMARY KEY IF NOT EXISTS (`uid`,`created`);
 
 COMMIT;
