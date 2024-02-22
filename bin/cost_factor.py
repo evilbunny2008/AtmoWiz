7cca6f0c99ac180ffcd7c66034099d5f06c77b6f
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-22 16:08:14 +1100

    Fixed up costfactor.py

--- bin/cost_factor.py
+++ bin/cost_factor.py
@@ -4,15 +4,20 @@ import configparser
 import MySQLdb
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 configParser = configparser.ConfigParser(allow_no_value = True)
-configParser.read("/etc/sensibo.conf")
+configParser.read("/etc/atmowiz.conf")
 hostname = configParser.get('mariadb', 'hostname', fallback = 'localhost')
-database = configParser.get('mariadb', 'database', fallback = 'sensibo')
-username = configParser.get('mariadb', 'username', fallback = 'sensibo')
+database = configParser.get('mariadb', 'database', fallback = 'atmowiz')
+username = configParser.get('mariadb', 'username', fallback = 'atmowiz')
 password = configParser.get('mariadb', 'password', fallback = 'password')
 
 mydb = MySQLdb.connect(hostname, username, password, database)
 
-query = "SELECT temperature, humidity, feelslike, mode, targetTemperature, fanLevel, amps FROM `sensibo` WHERE airconon = 1 AND amps != 0"
+query = "SELECT temperature, humidity, mode, targetTemperature, fanLevel, watts FROM `sensibo` WHERE airconon = 1 AND watts != 0"
 result_df = pd.read_sql(query, mydb)
 print(result_df)
+
+categorical_cols = ['mode', 'fanLevel']
