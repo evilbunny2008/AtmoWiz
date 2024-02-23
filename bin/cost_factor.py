d94bc3a5039337676719c3f6abcafae036822d46
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-24 00:59:47 +1100

    More cost_factor.py coding

--- bin/cost_factor.py
+++ bin/cost_factor.py
@@ -5,14 +5,7 @@ import MySQLdb
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import statsmodels.formula.api as smf
-
-from sklearn import datasets, linear_model
-
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.metrics import mean_squared_error, r2_score
 
 configParser = configparser.ConfigParser(allow_no_value = True)
 configParser.read("/etc/atmowiz.conf")
@@ -40,10 +33,8 @@ x_surf, y_surf = np.meshgrid(np.linspace(df.targetTemperature.min(), df.targetTe
 onlyX = pd.DataFrame({'targetTemperature': x_surf.ravel(), 'tempDiff': y_surf.ravel()})
 fittedY=results_formula.predict(exog=onlyX)
 
-## convert the predicted result in an array
 fittedY=np.array(fittedY)
 
-# Visualize the Data for Multiple Linear Regression
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(df['targetTemperature'], df['tempDiff'], df['watts'], c='red', marker='o', alpha=0.5)
@@ -51,48 +42,6 @@ ax.plot_surface(x_surf,y_surf,fittedY.reshape(x_surf.shape), color='b', alpha=0.
 ax.set_zlabel('watts')
 ax.set_xlabel('targetTemperature')
 ax.set_ylabel('tempDiff')
-plt.show()
 
 plt.title("Watts Vs tempDiff Vs temperature")
 plt.savefig("../web/out.png")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#categorical_cols = ['mode', 'fanLevel']
-#enc = OrdinalEncoder()
-#df[categorical_cols] = enc.fit_transform(df[categorical_cols])
-#print(df.head())
-#print(df['mode'].unique())
-#print(df.shape)
-#model = LinearRegression()
-#model.fit(X, Y, Z)
-#print(model.predict(X))
-#print('Intercept: ', model.intercept_)
-#print('Coefficients array: ', model.coef_)
-
-#sns.set_theme(style="ticks")
-
-#f, ax = plt.subplots(figsize=(10, 5))
-#sns.despine(f)
-#lmplot = sns.relplot(data=df, x="tempDiff", y="watts").fig
-#lmplot = sns.regplot(data=df, x="tempDiff", y="watts").figure
-#lmplot = sns.kdeplot(data=df, x="tempDiff", y="watts", cmap="Reds", shade=True, bw_method=.30).figure
-#lmplot = sns.kdeplot(data=df, x="tempDiff", y="watts").figure
-#lmplot = sns.regplot(data=df, x="tempDiff", y="watts").figure
-#lmplot = sns.scatterplot(data=df, x="tempDiff", y="watts", marker='+').figure
-
-#lmplot = sns.kdeplot(data=df, x="tempDiff", y="watts", shade=True).figure
-
