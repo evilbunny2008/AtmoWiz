f7e2271fadcc2c8291ed3bcd5ca7003a65335c54
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-02-25 23:43:03 +1100

    Tensorflow fixes

--- bin/LSTM.py
+++ bin/LSTM.py
@@ -2,8 +2,6 @@
 
 import configparser
 
-import matplotlib.pyplot as plt
-
 import numpy as np
 import pandas as pd
 
@@ -25,24 +23,28 @@ password = configParser.get('mariadb', 'password', fallback = 'password')
 db_uri = "mysql://%s:%s@%s/%s" % (username, password, hostname, database)
 engine = create_engine(db_uri)
 
-query = "SELECT temperature, (temperature - targetTemperature) as tempDiff, watts FROM `sensibo` WHERE airconon = 1 AND watts != 0 AND mode='cool'"
+query = "SELECT temperature, (temperature - targetTemperature) as tempDiff, humidity, feelsLike, watts FROM `sensibo` WHERE airconon = 1 AND watts > 0 AND mode='cool'"
 df = pd.read_sql(query, engine)
 
-X = df[["temperature", "tempDiff"]]
+X = df[["temperature", "tempDiff", "humidity", "feelsLike"]]
 y = df["watts"]
 
-X_new = np.reshape(X.values, (X.shape[0], X.shape[1], 1))
+X = np.reshape(X.values, (X.shape[0], X.shape[1], 1))
 
 model = Sequential()
-model.add(LSTM(50, input_shape=(X.shape[1], X.shape[2])))
+model.add(LSTM(50, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LSTM(50))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-model.fit(X, y, epochs=100, batch_size=32, verbose=1)
+model.fit(X, y, epochs=100, batch_size=32, verbose=0)
 
 temperature = float(sys.argv[1])
 temp_diff = float(sys.argv[2])
+humidity = float(sys.argv[3])
+feelsLike = float(sys.argv[4])
 
-X_new = np.array([[temperature, temp_diff]])
+X_new = np.array([[temperature, temp_diff, humidity, feelsLike]])
 X_new = np.reshape(X_new, (X_new.shape[0], X_new.shape[1], 1))
 
 prediction = model.predict(X_new)
