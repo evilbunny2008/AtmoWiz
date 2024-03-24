689d9c6fa52dcad4f83f51c465f6c3b3b2a2855a
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2024-03-24 16:03:25 +1100

    Add period cost

--- web/data.php
+++ web/data.php
@@ -512,14 +512,19 @@
 	$query = "SELECT round(sum(cost), 2) as cost FROM sensibo WHERE whentime >= '".date("Y-m-d 00:00:00")."' AND uid='$uid'";
 	$row = mysqli_fetch_assoc(mysqli_query($link, $query));
 	$currency_fmt = numfmt_create($currency_code, NumberFormatter::CURRENCY);
-	$row['cost'] = numfmt_format_currency($currency_fmt, $row['cost'], $currency);
+	$todaycost = numfmt_format_currency($currency_fmt, $row['cost'], $currency);
+
+	$query = "SELECT round(sum(cost), 2) as cost FROM sensibo WHERE uid='$uid' AND UNIX_TIMESTAMP(whentime) * 1000 >= $startTS AND UNIX_TIMESTAMP(whentime) * 1000 <= $startTS + $period";
+	$row = mysqli_fetch_assoc(mysqli_query($link, $query));
+	$currency_fmt = numfmt_create($currency_code, NumberFormatter::CURRENCY);
+	$periodcost = numfmt_format_currency($currency_fmt, $row['cost'], $currency);
 
 	$currsign = "C";
 	if($currtemp > 50)
 		$currsign = "F";
 
 	$commandHeader .= "<li class='card-demo10' style='text-align:center;cursor:pointer;' onClick='showDay(\"".(time() * 1000 - 86400000)."\"); return false;'><u><b>Current Conditions</b></u></li>\n";
-	$commandHeader .= "<li class='card-demo10'><b>".$currtime."</b> -- ".$currtemp."°$currsign, ".$currhumid."%, ".$row['cost']."</li>\n";
+	$commandHeader .= "<li class='card-demo10'><b>".$currtime."</b> -- ".$currtemp."°$currsign, ".$currhumid."%, $todaycost ($periodcost)</li>\n";
 	$commandHeader .= "<li class='card-demo10'>&nbsp;</li>\n";
 
 	$query = "SELECT *, DATE_FORMAT(whentime, '%H:%i') as wttime FROM weather ORDER BY whentime DESC LIMIT 1";
